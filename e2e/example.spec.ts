b80eccfafe3f0f178dd0627617b0f78a984b2ae6
Alik_Avizha <aleksandravizha@icloud.com>
2025-02-01 10:56:10 +0300

    feat(*): initialize project

---
+++ e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  // Переходим на страницу
+  await page.goto('http://localhost:3000/');
+
+  // Находим элемент по id
+  const element = page.locator('data-test-id=page-title');
+
+  // Проверяем, что элемент видим
+  await expect(element).toBeVisible();
+
+  // Пример проверки текста внутри элемента
+  await expect(element).toHaveText('Doggy spay');
+});
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,46 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+      "plugin:promise/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended" // Добавляем Prettier
+  ),
+  {
+    rules: {
+      // Примеры кастомных правил
+      "react/react-in-jsx-scope": "off", // Отключаем, так как Next.js автоматически добавляет React
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "promise/prefer-await-to-then": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "prettier/prettier": "error", // Включаем Prettier как правило ESLint
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect", // Автоматически определяет версию React
+      },
+      "import/resolver": {
+        typescript: {}, // Используем TypeScript для разрешения путей импортов
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
